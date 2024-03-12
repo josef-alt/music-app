45e6be19ea718f155c2ffbef5e671b0a493d3997
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-03-12 14:49:36 -0500

    changed back to imageview and set up default album art

--- src/main/java/controllers/MainPageController.java
+++ src/main/java/controllers/MainPageController.java
@@ -3,23 +3,70 @@ package controllers;
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
+import java.util.*;
 import java.util.stream.*;
 
 import javax.imageio.*;
 
+import javafx.beans.binding.*;
+import javafx.collections.*;
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.input.*;
 import javafx.scene.media.*;
+import javafx.scene.media.MediaPlayer.*;
+import javafx.stage.*;
 
 public class MainPageController {
 	@FXML
-	private MediaView album_art;
+	private ImageView album_art;
 
 	@FXML
 	private Label song_name, artist_name, album_name;
+
+	@FXML
+	private Button prev_button, pause_button, next_button;
+
+	private final Stage currentStage;
+	private MediaPlayer player;
+	private Media mediaElement;
+	private boolean paused;
+
+	public MainPageController() {
+		currentStage = new Stage();
+		currentStage.setResizable(false);
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/basic.fxml"));
+			loader.setController(this);
+			currentStage.setScene(new Scene(loader.load()));
+
+			System.out.println("finished constructor");
+		} catch (IOException e) {
+			System.out.println("error loading");
+			e.printStackTrace();
+		}
+	}
+
+	public void showStage() {
+		currentStage.showAndWait();
+	}
+
+	@FXML
+	public void initialize() {
+		paused = true;
+		prev_button.setOnAction(event -> {
+			handlePrevButton(event);
+		});
+		pause_button.setOnAction(event -> {
+			handlePauseButton(event);
+		});
+		next_button.setOnAction(event -> {
+			handleNextButton(event);
+		});
+	}
 
 	// this is just to test changing the image
 	private static int x = -1;
@@ -34,10 +81,24 @@ public class MainPageController {
 		}
 
 		String next = samples[x % samples.length];
-		// album_art.setImage(new Image("file:" + next));
-
 		// not really important for now, just proof of concept for my own sanity
 		song_name.setText(next.substring(next.lastIndexOf('\\') + 1));
+
+		mediaElement = new Media(new File(next).toURI().toString());
+		if (player != null)
+			player.dispose();
+		player = new MediaPlayer(mediaElement);
+
+		player.play();
+		if (paused)
+			player.pause();
+
+		setAlbumArt();
+	}
+
+	private void setAlbumArt() {
+		Image image = new Image(getClass().getResourceAsStream("/img/cd.png"));
+		album_art.setImage(image);
 	}
 
 	@FXML
@@ -50,6 +111,23 @@ public class MainPageController {
 	public void handlePauseButton(ActionEvent e) {
 		System.out.println("pause/play");
 
+		if (player == null) {
+			System.out.println("no player");
+		} else {
+			if (player.getStatus() == Status.READY) {
+				System.out.println("begin song");
+				player.play();
+				paused = false;
+			} else if (player.getStatus() == Status.PAUSED) {
+				System.out.println("hit play");
+				player.play();
+				paused = false;
+			} else {
+				System.out.println("hit pause");
+				player.pause();
+				paused = true;
+			}
+		}
 	}
 
 	@FXML
@@ -57,5 +135,4 @@ public class MainPageController {
 		System.out.println("next");
 		update(1);
 	}
-
 }
