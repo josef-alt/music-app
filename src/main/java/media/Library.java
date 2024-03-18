032015c8ec6f9b048d193179bf62fc8702462083
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-03-18 18:10:20 -0500

    added Library class to handle songs and playlists

---
+++ src/main/java/media/Library.java
@@ -0,0 +1,64 @@
+package media;
+
+import java.io.IOException;
+import java.nio.file.PathMatcher;
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Contains everything necessary for managing the music library.
+ * 
+ * TODO playlists, filtering, genre/artist/album/etc
+ */
+public class Library {
+	private Song[] files;
+	private Path directory;
+
+	// only supporting m4a files for now
+	private static final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.m4a");
+
+	public Library(String dir) {
+		this(Path.of(dir));
+	}
+
+	public Library(Path dir) {
+		this.directory = dir;
+
+		try {
+			loadFiles();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			this.files = new Song[0];
+		}
+	}
+
+	/**
+	 * Attempts to load files from selected directory
+	 * Filters out unsupported types
+	 */
+	private void loadFiles() throws IOException {
+		this.files = Files.list(directory).filter(matcher::matches).map(Path::toFile).map(Song::new)
+				.toArray(Song[]::new);
+	}
+
+	public int getNumberOfTracks() {
+		return files.length;
+	}
+
+	public Song getTrack(int trackNumber) {
+		return files[trackNumber];
+	}
+
+	public String toString() {
+		String list = Arrays.stream(files).map(Song::toString)
+				.collect(Collectors.joining(", ", "{", "}"));
+
+		return String.format("%s has %d files: %s", directory.toString(), files.length, list);
+	}
+}
