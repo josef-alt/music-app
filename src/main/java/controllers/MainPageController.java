e9c3364c232597b11429176563cba1a358aade65
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-03-18 19:01:20 -0500

    refactored controller

--- src/main/java/controllers/MainPageController.java
+++ src/main/java/controllers/MainPageController.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.prefs.*;
 import java.util.stream.*;
 
 import javax.imageio.*;
@@ -24,6 +25,7 @@ import javafx.scene.input.*;
 import javafx.scene.media.*;
 import javafx.scene.media.MediaPlayer.*;
 import javafx.stage.*;
+import media.*;
 
 public class MainPageController {
 	@FXML
@@ -39,21 +41,36 @@ public class MainPageController {
 	private MenuItem quit_button;
 
 	private final Stage currentStage;
-	private MediaPlayer player;
-	private File currentFile;
-	private boolean paused;
+	private final Player player;
+
+	private Preferences userSettings;
 
 	public MainPageController() {
 		currentStage = new Stage();
 		currentStage.setResizable(false);
+
+		player = new Player();
+
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/basic.fxml"));
 			loader.setController(this);
-			currentStage.setScene(new Scene(loader.load()));
+
+			Scene currentScene = new Scene(loader.load());
+
+			// configure layout
+			currentScene.getStylesheets().add(getClass().getResource("/themes/basic_config.css").toString());
+
+			// configure color scheme - should not alter layout
+			currentScene.getStylesheets().add(getClass().getResource("/themes/1500.css").toString());
+
+			currentStage.setScene(currentScene);
+
+			// ensure that the first track is displayed properly
+			setTrackInformation();
 
 			System.out.println("finished constructor");
 		} catch (IOException e) {
-			System.out.println("error loading");
+			System.err.println("Failed to load fxml");
 			e.printStackTrace();
 		}
 	}
@@ -64,149 +81,73 @@ public class MainPageController {
 
 	private ImageView play_icon, prev_icon, next_icon;
 	private Image play, pause;
+
 	@FXML
+	/**
+	 * Configures button handlers and loads icons.
+	 */
 	public void initialize() {
-		paused = true;
+		// load required resources
+		play = new Image(getClass().getResourceAsStream("/img/50/play.png"));
+		pause = new Image(getClass().getResourceAsStream("/img/50/pause.png"));
 
-		play = new Image(getClass().getResourceAsStream("/img/play-50.png"));
-		pause = new Image(getClass().getResourceAsStream("/img/pause-50.png"));
-		prev_icon = new ImageView(new Image(getClass().getResourceAsStream("/img/prev-50.png")));
+		prev_icon = new ImageView(new Image(getClass().getResourceAsStream("/img/50/prev.png")));
 		play_icon = new ImageView(play);
-		next_icon = new ImageView(new Image(getClass().getResourceAsStream("/img/next-50.png")));
+		next_icon = new ImageView(new Image(getClass().getResourceAsStream("/img/50/next.png")));
 
-		prev_button.setStyle("-fx-background-color: transparent;");
+		// set up icons
 		prev_button.setGraphic(prev_icon);
-		pause_button.setStyle("-fx-background-color: transparent;");
 		pause_button.setGraphic(play_icon);
-		next_button.setStyle("-fx-background-color: transparent;");
 		next_button.setGraphic(next_icon);
 
-		prev_button.setOnAction(event -> {
-			handlePrevButton(event);
-		});
+		// configure controls
+		prev_button.setOnAction(event -> player.prevSong());
 		pause_button.setOnAction(event -> {
-			handlePauseButton(event);
-		});
-		next_button.setOnAction(event -> {
-			handleNextButton(event);
-		});
-
-		quit_button.setOnAction(event -> {
-			quit();
-		});
-	}
-
-	// this is just to test changing the image
-	private static int x = -1;
-	private static String[] samples = Stream.of(new File("E:/Media/Music/with art/").listFiles())
-			.filter(f -> !f.isDirectory()).map(File::toString).toArray(String[]::new);
-
-	// cylce through sample images on key press
-	public void update(int delta) {
-		x += delta;
-		if (x < 0) {
-			x += samples.length;
-		}
-
-		String next = samples[x % samples.length];
-		// not really important for now, just proof of concept for my own sanity
-		song_name.setText(next.substring(next.lastIndexOf('\\') + 1));
-
-		currentFile = new File(next);
-		Media mediaElement = new Media(currentFile.toURI().toString());
-		if (player != null)
-			player.dispose();
-		player = new MediaPlayer(mediaElement);
-
-		player.play();
-		if (paused)
 			player.pause();
+			if (player.isPaused()) {
+				play_icon.setImage(play);
+			} else {
+				play_icon.setImage(pause);
+			}
+		});
+		next_button.setOnAction(event -> player.nextSong());
 
-		setTrackInformation();
+		// update user interface
+		player.addEvent(() -> setTrackInformation());
+
+		// exit safely
+		quit_button.setOnAction(event -> quit());
 	}
 
+	/**
+	 * Update track information in user interface.
+	 */
 	private void setTrackInformation() {
-		try {
-			AudioFile af = AudioFileIO.read(currentFile);
-			Tag tag = af.getTag();
+		System.out.println("event " + player.getSong());
 
-			if (tag.hasField(FieldKey.ALBUM)) {
-				album_name.setText(tag.getFirst(FieldKey.ALBUM));
-			}
-
-			Image image;
-			if (tag.hasField(FieldKey.COVER_ART)) {
-				image = new Image(new ByteArrayInputStream(tag.getFirstArtwork().getBinaryData()), 250, 250, false,
-						false);
-			} else {
-				image = new Image(getClass().getResourceAsStream("/img/cd.png"));
-			}
-			album_art.setImage(image);
-
-			if (tag.hasField(FieldKey.ARTIST)) {
-				artist_name.setText(tag.getFirst(FieldKey.ARTIST));
-			}
-
-			if (tag.hasField(FieldKey.TITLE)) {
-				song_name.setText(tag.getFirst(FieldKey.TITLE));
-			}
-
-		} catch (CannotReadException | IOException | TagException | ReadOnlyFileException
-				| InvalidAudioFrameException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@FXML
-	public void handlePrevButton(ActionEvent e) {
-		System.out.println("prev");
-		update(-1);
-	}
-
-	@FXML
-	public void handlePauseButton(ActionEvent e) {
-		System.out.println("pause/play");
-
-		if (player == null) {
-			System.out.println("no player");
-		} else {
-			if (player.getStatus() == Status.READY) {
-				System.out.println("begin song");
-				player.play();
-				paused = false;
-			} else if (player.getStatus() == Status.PAUSED) {
-				System.out.println("hit play");
-				player.play();
-				paused = false;
-			} else {
-				System.out.println("hit pause");
-				player.pause();
-				paused = true;
-			}
+		Song currentlyPlaying = player.getSong();
+		if (currentlyPlaying.hasAlbum()) {
+			album_name.setText(currentlyPlaying.getAlbum());
 		}
 
-		if (paused) {
-			play_icon.setImage(play);
-		} else {
-			play_icon.setImage(pause);
+		if (currentlyPlaying.hasArtist()) {
+			artist_name.setText(currentlyPlaying.getArtist());
 		}
+
+		if (currentlyPlaying.hasTitle()) {
+			song_name.setText(currentlyPlaying.getTitle());
+		}
+
+		// no check because it should never be null
+		album_art.setImage(currentlyPlaying.getCover());
 	}
 
-	@FXML
-	public void handleNextButton(ActionEvent e) {
-		System.out.println("next");
-		update(1);
-	}
-
+	/**
+	 * Ensures player is closed properly and exits stage.
+	 */
 	private void quit() {
 		System.out.println("quitting");
-		if (player != null) {
-			if (player.getStatus() == Status.PLAYING) {
-				player.stop();
-			}
-		}
-
+		player.quit();
 		currentStage.close();
 	}
 }
