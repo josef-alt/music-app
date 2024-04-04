642257e19ca6e3bc077724a2c9251538a6658bce
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-04-04 16:04:02 -0500

    updated sidebar

--- src/main/java/controllers/SidebarController.java
+++ src/main/java/controllers/SidebarController.java
@@ -1,22 +1,46 @@
 package controllers;
 
-import javafx.collections.*;
-import javafx.fxml.*;
-import javafx.scene.control.*;
-import media.*;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
+import util.ThemeSwitcher;
 
+/**
+ * Controls for the tabbed side-bar with the currently playing queue
+ * and filters for artist/album/genre
+ */
 public class SidebarController {
 	@FXML
 	private ListView sideview_songs;
 
-	private Model model;
+	@FXML
+	private Tab artists_tab, albums_tab, genres_tab;
 
-	public SidebarController(Model model) {
+	private Model model;
+	private ThemeSwitcher switcher;
+
+	public SidebarController(Model model, ThemeSwitcher switcher) {
 		this.model = model;
+		this.switcher = switcher;
 	}
 
 	@FXML
 	public void initialize() {
+		// set up tab icons and link to theme switcher
+		ImageView artist_icon = new ImageView();
+		ImageView album_icon = new ImageView();
+		ImageView genre_icon = new ImageView();
+		switcher.addView(artist_icon, "artist", 20);
+		switcher.addView(album_icon, "album", 20);
+		switcher.addView(genre_icon, "genre", 20);
+
+		artists_tab.setGraphic(artist_icon);
+		albums_tab.setGraphic(album_icon);
+		genres_tab.setGraphic(genre_icon);
+
+		// link user selections to player
 		sideview_songs.getSelectionModel().selectedIndexProperty().addListener((obs, old, sel) -> {
 			int index = sel.intValue();
 			if (index >= 0)
@@ -24,8 +48,9 @@ public class SidebarController {
 		});
 
 		sideview_songs.setItems(model.getPlayer().getObservableList());
-		sideview_songs.setPrefHeight(425);
+		sideview_songs.setPrefHeight(400);
 
+		// link auto-play to list view
 		model.getPlayer().addListener(() -> {
 			sideview_songs.getSelectionModel().select(model.getPlayer().getSong());
 		});
