a48e7ce3a35845a5b056ad0e034f4b5a2e7381ed
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-04-11 00:01:47 -0500

    added playlist tab, basic playlist structure

---
+++ src/main/java/media/Playlist.java
@@ -0,0 +1,26 @@
+package media;
+
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Basic play list structure to be expanded later
+ */
+public class Playlist {
+	private String name;
+	private ObservableList<Song> songs;
+
+	public Playlist(String name, List<Song> songs) {
+		this.name = name;
+		this.songs = FXCollections.observableArrayList(songs);
+	}
+
+	public ObservableList<Song> getSongs() {
+		return songs;
+	}
+
+	public String toString() {
+		return name;
+	}
+}
